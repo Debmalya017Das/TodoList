f33db8985130543929913c2de8b9910e40a5fbae
Debmalya Das <debmalya017@gmail.com>
2024-06-15 00:21:06 +0530

    working

---
+++ src/components/TodoForm.js
@@ -0,0 +1,23 @@
+import React, {useState} from 'react'
+
+export const TodoForm = ({addTodo}) => {
+    const [value, setValue] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (value) {
+          addTodo(value);
+          setValue('');
+        }
+        if (!value.trim()) {
+          alert('Task cannot be empty');
+          return;
+    }
+      };
+  return (
+    <form onSubmit={handleSubmit} className="TodoForm">
+    <input type="text" value={value} onChange={(e) => setValue(e.target.value)} className="todo-input" placeholder='What is the task today?' />
+    <button type="submit" className='todo-btn'>Add Task</button>
+  </form>
+  )
+}
